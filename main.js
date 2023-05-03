4f07924609aafbe0e5d9fa93929f9dd2813c5715
Istiak165 <khankawser123@gmail.com>
2023-05-03 19:53:14 +0600

    modal create with functional way

--- main.js
+++ main.js
@@ -3,29 +3,43 @@ const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector('.close');
 const overlay = document.querySelector(".overlay");
 
+function modalShow(){
+    modal.style.opacity = 1;
+    modal.style.visibility = "visible"
+}
+
+function modalHidden() {
+    modal.style.opacity = 0;
+    modal.style.visibility = "hidden"
+}
+
+
+function overlayShow(){
+    overlay.style.opacity = 1;
+    overlay.style.visibility = "visible"
+}
+
+function overlayHidden() {
+    overlay.style.opacity = 0;
+    overlay.style.visibility = "hidden"
+}
+
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function (){
-        modal.style.opacity = 1;
-        modal.style.visibility = "visible"
-
-        overlay.style.opacity = 1;
-        overlay.style.visibility = "visible"
+        modalShow();
+        overlayShow();
     })
 }
 
-overlay.addEventListener('click', function (){
-        modal.style.opacity = 0;
-        modal.style.visibility = "hidden"
 
-        overlay.style.opacity = 0;
-        overlay.style.visibility = "hidden"
+closeBtn.addEventListener('click', function (){
+    modalHidden();
+    overlayHidden()
+
 })
 
 
-closeBtn.addEventListener('click', function (){
-    modal.style.opacity = 0;
-    modal.style.visibility = "hidden"
-
-    overlay.style.opacity = 0;
-    overlay.style.visibility = "hidden"
+overlay.addEventListener('click', function (){
+    modalHidden();
+    overlayHidden();
 })
